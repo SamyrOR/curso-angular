c733d35da8d4cce84a376352b50b15ba5bd75a9c
Samyr O. Ribeiro <saamyr@live.com>
2020-11-22 23:22:22 -0300

    trabalhando com diversas formas de input no angular

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -1,7 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+  FormArray,
+} from '@angular/forms';
+import { Observable } from 'rxjs';
+import { FormValidations } from '../shared/form-validations';
 import { EstadoBr } from '../shared/models/estado-br';
+import { ConsultaCepService } from '../shared/services/consulta-cep.service';
 import { DropdownService } from '../shared/services/dropdown.service';
 
 @Component({
@@ -11,12 +20,17 @@ import { DropdownService } from '../shared/services/dropdown.service';
 })
 export class DataFormComponent implements OnInit {
   formulario: FormGroup;
-  estados: EstadoBr[] = [];
+  estados: Observable<EstadoBr[]>;
+  cargos: any[];
+  tecnologias: any[];
+  newsletterOpt: any[];
 
+  frameworks = ['Angular', 'React', 'Vue', 'Sencha'];
   constructor(
     private formBuilder: FormBuilder,
     private http: HttpClient,
-    private dropDown: DropdownService
+    private dropDown: DropdownService,
+    private cepService: ConsultaCepService
   ) {}
 
   ngOnInit(): void {
@@ -44,17 +58,37 @@ export class DataFormComponent implements OnInit {
         cidade: [null, Validators.required],
         estado: [null, Validators.required],
       }),
+      cargo: [null],
+      tecnologias: [null],
+      newsletter: ['s'],
+      termos: [null, Validators.pattern('true')],
+      frameworks: this.buildFrameworks(),
     });
-    this.dropDown.getEstadosBr().subscribe((res: EstadoBr) => {
-      console.log(res);
-      this.estados.push(res);
-    });
+    this.estados = this.dropDown.getEstadosBr();
+    this.cargos = this.dropDown.getCargos();
+    this.tecnologias = this.dropDown.getTecs();
+    this.newsletterOpt = this.dropDown.getNewsletter();
+  }
+
+  buildFrameworks() {
+    const values = this.frameworks.map((v) => new FormControl(false));
+    return this.formBuilder.array(
+      values,
+      FormValidations.requiredMinCheckbox(2)
+    );
   }
 
   onSubmit() {
+    let valueSubmit = Object.assign({}, this.formulario.value);
+    valueSubmit = Object.assign(valueSubmit, {
+      frameworks: valueSubmit.frameworks
+        .map((v, i) => (v ? this.frameworks[i] : null))
+        .filter((v) => v !== null),
+    });
+    console.log(valueSubmit);
     if (this.formulario.valid) {
       this.http
-        .post('https://httpbin.org/post', JSON.stringify(this.formulario.value))
+        .post('https://httpbin.org/post', JSON.stringify(valueSubmit))
         .subscribe(
           (dados) => {
             console.log(dados);
@@ -92,16 +126,10 @@ export class DataFormComponent implements OnInit {
 
   consultaCEP() {
     let cep = this.formulario.get('endereco.cep').value;
-    cep = cep.replace(/\D/g, '');
-    if (cep != '') {
-      let validacep = /^[0-9]{8}$/;
-      if (validacep.test(cep)) {
-        this.http
-          .get(`//viacep.com.br/ws/${cep}/json`)
-          .subscribe((dados) => this.populaDadosForm(dados));
-      }
-    } else if (cep === null) {
-      alert('Digite um valor válido');
+    if (cep != null && cep !== '') {
+      this.cepService
+        .consultaCEP(cep)
+        .subscribe((dados) => this.populaDadosForm(dados));
     }
   }
   populaDadosForm(dados) {
@@ -144,5 +172,17 @@ export class DataFormComponent implements OnInit {
         estado: null,
       },
     });
+  }
+
+  setarCargo() {
+    const cargo = { nome: 'Dev', nivel: 'Junior', desc: 'Dev  Pl' };
+    this.formulario.get('cargo').setValue(cargo);
+  }
+  comparaObjetos(obj1, obj2) {
+    return obj1 && obj2 ? obj1.nome === obj2.nome : obj1 === obj2;
+  }
+
+  setarTecnologias() {
+    this.formulario.get('tecnologias').setValue(['html', 'css', 'js']);
   }
 }
